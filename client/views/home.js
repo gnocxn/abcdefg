f3379d902c2bead78c94033be6accef4e473ee06
Nguyen Xuan Cong <nguyenxuancong@outlook.com>
2015-09-23 18:42:04 +0700

    create method auto followers

--- client/views/home.js
+++ client/views/home.js
@@ -20,5 +20,14 @@ Template.home.events({
             FlashMessages.sendSuccess(rs, { hideDelay: 5000 });
             t.disabledButton.set(false);
         })
+    },
+    'click #btnFollowFromFollowers' : function(e,t){
+        e.preventDefault();
+        t.disabledButton.set(true);
+        Meteor.call('tblr_autoFollowingFromFollowers', function(err, rs){
+            if(err) FlashMessages.sendError(err, { hideDelay: 2000 });
+            FlashMessages.sendSuccess(rs, { hideDelay: 10000 });
+            t.disabledButton.set(false);
+        })
     }
 })
--- server/___.js
+++ server/___.js
@@ -66,7 +66,7 @@ if (Meteor.isServer) {
                 var tmp = TMP.fileSync({mode: 0644, prefix: 'ph_', postfix: '.mp4'});
                 console.log('created and writing : ', tmp.name);
                 fs.writeFileSync(tmp.name, ab.body);
-                var caption = _.template('<p>Hey,see more <a target="_blank" href="http://p0rnhunt.tumblr.com/full?viewkey=<%=fullId%>"><%=title%></a><br/>(via <a target="_blank" href="http://p0rnhunt.tumblr.com">pornhunt.xyz</a>)</p>');
+                var caption = _.template('<p>Hey, see more <a target="_blank" href="http://p0rnhunt.tumblr.com/full?viewkey=<%=fullId%>"><%=title%></a><br/>(via <a target="_blank" href="http://p0rnhunt.tumblr.com">pornhunt.xyz</a>)</p>');
                 var tags = _.union(clip.tags, ['pornhunt.xyz']);
                 var options = {
                     state: state || 'published',
@@ -82,15 +82,18 @@ if (Meteor.isServer) {
                     })
                 })
                 tmp.removeCallback();
-                var newDate = new Date;
-                var aff = Posts.upsert({fullId: clip.fullId, type: 'ph_shortVideo'}, {
-                    fullId: clip.fullId,
-                    tumblr_pId: rs.result.id.toString(),
-                    blogName: blogName,
-                    type: 'ph_shortVideo',
-                    updatedAt: newDate
-                });
-                return aff;
+                if(rs.result && rs.result.id){
+                    var newDate = new Date;
+                    var aff = Posts.upsert({fullId: clip.fullId, type: 'ph_shortVideo'}, {
+                        fullId: clip.fullId,
+                        tumblr_pId: rs.result.id.toString(),
+                        blogName: blogName,
+                        type: 'ph_shortVideo',
+                        updatedAt: newDate
+                    });
+                    return aff;
+                }
+
             }
             return false;
         },
@@ -105,8 +108,8 @@ if (Meteor.isServer) {
         tblr_followers: function (blogName, limit, offset) {
             check(blogName, String);
             var options = {
-                limit : limit || 20,
-                offset : offset || 0
+                limit: limit || 20,
+                offset: offset || 0
             }
             var rs = Async.runSync(function (done) {
                 TumblrClient.followers(blogName, options, function (err, data) {
@@ -142,9 +145,28 @@ if (Meteor.isServer) {
         tblr_getPosts: function (blogName) {
 
         },
+        tblr_doFollow: function (url) {
+            try {
+                Meteor.sleep(2000);
+                var rs = Async.runSync(function (DONE) {
+                    TumblrClient.follow(url, function (err, data) {
+                        //console.log(err,data);
+                        if (err) {
+                            console.log(err);
+                            DONE(null, err);
+                        } else {
+                            DONE(null, data);
+                        }
+                    });
+                });
+                return rs.result;
+            } catch (ex) {
+                console.log(ex)
+            }
+        },
         tblr_updateAllInformation: function () {
             var rs = Async.runSync(function (DONE) {
-                try{
+                try {
                     async.waterfall([
                         Meteor.bindEnvironment(function (cbFollowing) {
                             var result = Meteor.call('tblr_userInfo');
@@ -177,88 +199,138 @@ if (Meteor.isServer) {
                                     });
                                 });
 
-                                var total_Followers = (blogsInfor.length == 1) ? blogsInfor[0].followers : _.reduce(blogsInfor, function(a,b){return a.followers + b.followers});
+                                var total_Followers = (blogsInfor.length == 1) ? blogsInfor[0].followers : _.reduce(blogsInfor, function (a, b) {
+                                    return a.followers + b.followers
+                                });
                                 cbFollowing(null, {
                                     username: user.name,
-                                    following : user.following,
+                                    following: user.following,
                                     blogsInfor: blogsInfor,
-                                    msg1 : 'Updated UserInfo:'+user.name+' (following : ' + user.following + ', ' + blogsInfor.length + ' blog (s) has '+ total_Followers + ' followers), '
+                                    msg1: 'Updated UserInfo:' + user.name + ' (following : ' + user.following + ', ' + blogsInfor.length + ' blog (s) has ' + total_Followers + ' followers), '
                                 });
                             } else {
                                 throw new Meteor.Error('No User Info')
                             }
                         }),
                         Meteor.bindEnvironment(function (userInfo, cbFollowers) {
-                            if(userInfo && userInfo.following){
-                                //console.log('getFollowing', userInfo);
-                                var offsets = getOffsets(userInfo.following, 20);
-                                async.concat(offsets, function(offset, cbFollowing){
+                            if (userInfo && userInfo.following) {
+                                var current_totalFollowing = Followings.find({username: userInfo.username}).count(),
+                                    total_new = Math.abs(userInfo.following - current_totalFollowing);
+                                var offsets = getOffsets(total_new, 20);
+                                async.concat(offsets, function (offset, cbFollowing) {
                                     var result = Meteor.call('tblr_following', 20, offset);
                                     var blogs = (result.blogs) ? result.blogs : [];
                                     cbFollowing(null, blogs);
-                                }, function(err, following){
-                                    var updatedCount = 0 ;
-                                    _.each(following, function(f){
-                                        var isExists = Followings.findOne({name : f.name});
-                                        if(!isExists){
+                                }, function (err, following) {
+                                    var updatedCount = 0;
+                                    _.each(following, function (f) {
+                                        var isExists = Followings.findOne({name: f.name});
+                                        if (!isExists) {
                                             var insertedAt = new Date();
-                                            f = _.extend(f, {username : userInfo.username, insertedAt : insertedAt});
+                                            f = _.extend(f, {username: userInfo.username, insertedAt: insertedAt});
                                             Followings.insert(f);
                                             updatedCount++;
                                         }
                                     });
-                                    cbFollowers(null,{
-                                        msg : userInfo.msg1 + 'Inserted ' + updatedCount + ' Followings, ',
-                                        blogsInfor : userInfo.blogsInfor
+                                    cbFollowers(null, {
+                                        msg: userInfo.msg1 + 'Inserted ' + updatedCount + ' Followings, ',
+                                        blogsInfor: userInfo.blogsInfor,
+                                        username: userInfo.username
                                     });
                                 })
-                            }else{
+                            } else {
                                 throw new Meteor.Error('No following')
                             }
                         }),
-                        Meteor.bindEnvironment(function(followers, cbDone){
-                            if(followers && followers.blogsInfor){
+                        Meteor.bindEnvironment(function (followers, cbDone) {
+                            if (followers && followers.blogsInfor) {
                                 //console.log('getFollowers', followers);
-                                async.concat(followers.blogsInfor, function(blog, cbBlog){
-                                    var offsets = getOffsets(blog.followers, 20);
-                                    async.concat(offsets,function(offset, cbBlogFollowers){
+                                async.concat(followers.blogsInfor, function (blog, cbBlog) {
+                                    var current_totalFollowers = Followers.find({blogName: blog.blogName}).count();
+                                    var total_new = Math.abs(blog.followers - current_totalFollowers);
+                                    var offsets = getOffsets(total_new, 20);
+                                    async.concat(offsets, function (offset, cbBlogFollowers) {
                                         var result = Meteor.call('tblr_followers', blog.blogName, 20, offset);
                                         cbBlogFollowers(null, result.users);
-                                    },function(err, users){
-                                        if(err) throw new Meteor.Error(err);
+                                    }, function (err, users) {
+                                        if (err) throw new Meteor.Error(err);
                                         var updatedCount = 0;
                                         //console.log('total followers : ', users.length);
-                                        _.each(users, function(u){
-                                            var isExists = Followers.findOne({blogName : blog.blogName, name : u.name});
-                                            if(!isExists){
+                                        _.each(users, function (u) {
+                                            var isExists = Followers.findOne({blogName: blog.blogName, name: u.name});
+                                            if (!isExists) {
                                                 var insertedAt = new Date();
-                                                u = _.extend(u, {blogName : blog.blogName, insertedAt : insertedAt});
+                                                u = _.extend(u, {
+                                                    username: followers.username,
+                                                    blogName: blog.blogName,
+                                                    insertedAt: insertedAt
+                                                });
                                                 Followers.insert(u);
                                                 updatedCount++;
                                                 //console.log('inserted follower', updatedCount);
+                                            }else{
+                                                var updatedAt = new Date();
+                                                Followers.update({_id : isExists._id},{
+                                                    following : u.following,
+                                                    updatedAt : updatedAt
+                                                });
                                             }
                                         });
-                                        cbBlog(null, 'Blog '+ blog.blogName + ' inserted new ' + updatedCount + ' followers ;');
+                                        cbBlog(null, 'Blog ' + blog.blogName + ' inserted new ' + updatedCount + ' followers ;');
                                     })
-                                },function(err,msgs){
-                                    if(err) throw new Meteor.Error(err);
+                                }, function (err, msgs) {
+                                    if (err) throw new Meteor.Error(err);
                                     cbDone(null, followers.msg + msgs.join(','));
                                 })
-                            }else{
+                            } else {
                                 throw new Meteor.Error('No followers')
                             }
                         })
-                    ],Meteor.bindEnvironment(function(err, finished){
-                        if(err) throw new Meteor.Error(err);
+                    ], Meteor.bindEnvironment(function (err, finished) {
+                        if (err) throw new Meteor.Error(err);
                         console.log('tblr_updateAllInformation:', finished)
                         DONE(null, finished);
                     }))
-                }catch(ex){
+                } catch (ex) {
                     console.log(ex)
                 }
 
             });
             return rs.result;
+        },
+        tblr_autoFollowingFromFollowers: function (blogName) {
+            var blogName = blogName || 'p0rnhunt.tumblr.com';
+            var unFollowing = Followers.find({$and: [{blogName: blogName}, {following: false}]}, {limit: 50}).fetch();
+            if (unFollowing.length > 0) {
+                var rs = Async.runSync(function (done) {
+                    async.concat(unFollowing,
+                        Meteor.bindEnvironment(function (f, cbSuccess) {
+                            var res = Meteor.call('tblr_doFollow', f.url);
+                            //console.log(res);
+                            if(res){
+                                var updatedAt = new Date();
+                                Followers.update({_id : f._id},{
+                                    $set : {
+                                        following : true,
+                                        updatedAt : updatedAt
+                                    }
+                                });
+                                cbSuccess(null, 1);
+                            }
+                        }), Meteor.bindEnvironment(function (err, result) {
+                            if (err) console.log(err);
+                            var totalUpdated = _.reduce(result, function (a, b) {
+                                    return a + b
+                                }) || 0;
+                            var msg = 'Now following ' + totalUpdated + ' from followers.';
+                            console.log('tblr_autoFollowingFromFollowers:',msg);
+                            done(null, msg);
+                        }))
+                });
+                return rs.result;
+            } else {
+                return 'No un-following from followers!'
+            }
         },
         ali_importBestSellingProducts: function (spreadsheetId, gridId, isDeleteOlder) {
             var isDeleteOlder = isDeleteOlder || false;
@@ -370,10 +442,10 @@ if (Meteor.isServer) {
     });
 
     SyncedCron.add({
-        name: 'Every 1 hours update all information',
+        name: 'Every 2 hours update all information',
         schedule: function (parser) {
             // parser is a later.parse object
-            return parser.text('every 60 mins');
+            return parser.text('every 2 hours');
         },
         job: function () {
             var aff = Meteor.call('tblr_updateAllInformation');
