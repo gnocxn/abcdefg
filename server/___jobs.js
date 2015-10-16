48ba79024e2cabb912421efd8518ec08b4033ea8
Nguyen Xuan Cong <nguyenxuancong@outlook.com>
2015-10-16 14:44:49 +0700

    jobs ui

--- server/___jobs.js
+++ server/___jobs.js
@@ -1,21 +1,31 @@
 if (Meteor.isServer) {
-    myJobs.processJobs('fetch_gifs_pornhub_step1', function (job, cb) {
+    myJobs.processJobs('fetch_gifs_pornhub_step1',{
+        concurrency: 2,
+        payload: 1,
+        pollInterval: 5000,
+        prefetch: 1
+    }, function (job, cb) {
         var page = job.data.page;
         console.log('JOB : Fetch Step 1 started', page);
         var ids = Meteor.call('fetch_gifs_pornhub_step1', page);
         if (ids.length > 0) {
             _.each(ids, function (id) {
                 var newJob = new Job(myJobs, 'fetch_gifs_pornhub_step2', {_gId: id});
-                var second = _.random(40, 80);
+                var second = _.random(40, 70);
                 newJob.delay(second * 1000).save();
             });
-            console.log('JOB : Fetch Step 1 finished', page);
-            job.done();
-            cb();
         }
+        console.log('JOB : Fetch Step 1 finished', page);
+        job.done();
+        cb();
     });
 
-    myJobs.processJobs('fetch_gifs_pornhub_step2', function (job, cb) {
+    myJobs.processJobs('fetch_gifs_pornhub_step2',{
+        concurrency: 7,
+        payload: 1,
+        pollInterval: 5000,
+        prefetch: 1
+    }, function (job, cb) {
         var id = job.data._gId;
         console.log('JOB : Fetch Step 2 started', id);
         var gId = Meteor.call('fetch_gifs_pornhub_step2', id);
@@ -29,7 +39,12 @@ if (Meteor.isServer) {
         }
     });
 
-    myJobs.processJobs('fetch_gifs_pornhub_updateTags', function (job, cb) {
+    myJobs.processJobs('fetch_gifs_pornhub_updateTags',{
+        concurrency: 5,
+        payload: 1,
+        pollInterval: 5000,
+        prefetch: 1
+    }, function (job, cb) {
         var id = job.data._gId;
         console.log('JOB : Fetch tags started', id);
         var result = Meteor.call('fetch_gifs_pornhub_updateTags', id);
---
+++ server/___publish-count.js
@@ -0,0 +1,61 @@
+if(Meteor.isServer){
+    Meteor.publish('total_jobs_step1',function(){
+        Counts.publish(this, 'total_jobs_step1',myJobs.find({type : 'fetch_gifs_pornhub_step1'}));
+    });
+
+    Meteor.publish('total_jobs_step1_ready',function(){
+        Counts.publish(this, 'total_jobs_step1_ready',myJobs.find({type : 'fetch_gifs_pornhub_step1', status : 'ready'}));
+    });
+
+    Meteor.publish('total_jobs_step1_running',function(){
+        Counts.publish(this, 'total_jobs_step1_running',myJobs.find({type : 'fetch_gifs_pornhub_step1', status : 'running'}));
+    });
+
+    Meteor.publish('total_jobs_step1_completed',function(){
+        Counts.publish(this, 'total_jobs_step1_completed',myJobs.find({type : 'fetch_gifs_pornhub_step1', status : 'completed'}));
+    });
+
+    Meteor.publish('total_jobs_step2',function(){
+        Counts.publish(this, 'total_jobs_step2',myJobs.find({type : 'fetch_gifs_pornhub_step2'}));
+    });
+
+    Meteor.publish('total_jobs_step2_ready',function(){
+        Counts.publish(this, 'total_jobs_step2_ready',myJobs.find({type : 'fetch_gifs_pornhub_step2', status : 'ready'}));
+    });
+
+    Meteor.publish('total_jobs_step2_running',function(){
+        Counts.publish(this, 'total_jobs_step2_running',myJobs.find({type : 'fetch_gifs_pornhub_step2',status : 'running'}));
+    });
+
+    Meteor.publish('total_jobs_step2_completed',function(){
+        Counts.publish(this, 'total_jobs_step2_completed',myJobs.find({type : 'fetch_gifs_pornhub_step2', status : 'completed'}));
+    });
+
+    Meteor.publish('total_jobs_tags',function(){
+        Counts.publish(this, 'total_jobs_tags',myJobs.find({type : 'fetch_gifs_pornhub_updateTags'}));
+    });
+
+    Meteor.publish('total_jobs_tags_ready',function(){
+        Counts.publish(this, 'total_jobs_tags_ready',myJobs.find({type : 'fetch_gifs_pornhub_updateTags', status : 'ready'}));
+    });
+
+    Meteor.publish('total_jobs_tags_running',function(){
+        Counts.publish(this, 'total_jobs_tags_running',myJobs.find({type : 'fetch_gifs_pornhub_updateTags',status : 'running'}));
+    });
+
+    Meteor.publish('total_jobs_tags_completed',function(){
+        Counts.publish(this, 'total_jobs_tags_completed',myJobs.find({type : 'fetch_gifs_pornhub_updateTags', status : 'completed'}));
+    });
+
+    Meteor.publish('total_gifs',function(){
+        Counts.publish(this, 'total_gifs',PORNHUBGIFS.find());
+    });
+
+    Meteor.publish('total_gifs_completed',function(){
+        Counts.publish(this, 'total_gifs_completed',PORNHUBGIFS.find({isFetchDone : true}));
+    });
+
+    Meteor.publish('total_movies',function(){
+        Counts.publish(this, 'total_movies',PORNHUBMOVIES.find());
+    });
+}
